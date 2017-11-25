fec3a690a792cf00fa1f5807a469e5c94489b6a2
Ioannis Kappas <ikappas@devworks.gr>
2017-11-25 14:17:57 +0200

    Update sass-loader rewire

--- packages/react-app-rewire-sass/index.js
+++ packages/react-app-rewire-sass/index.js
@@ -1,20 +1,54 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const { getFileLoader, getCssRules } = require('react-app-rewired');
 
-const urlLoader = function (conf) {
-  return conf.loader === 'url';
-};
+function createRewireSass(sassLoaderOptions = {}) {
+  return function (config, env) {
+    const sassExtension = /\.scss$/;
 
-function rewireSass(config, env) {
-  config.module.loaders.find(urlLoader).exclude.push(/\.scss$/);
+    const fileLoader = getFileLoader(config.module.rules);
+    fileLoader.exclude.push(sassExtension);
 
-  config.module.loaders.push({
-    test: /\.scss$/,
-    loader: (env === 'development')
-      ? 'style!css!sass'
-      : ExtractTextPlugin.extract('style', 'css!sass')
-  });
+    const cssRules = getCssRules(config.module.rules);
 
-  return config;
+    let sassRules;
+    if (env === 'production') {
+      sassRules = {
+        test: sassExtension,
+        loader: [
+          // TODO: originally this part is wrapper in extract-text-webpack-plugin
+          //       which we cannot do, so some things like relative publicPath
+          //       will not work.
+          //       https://github.com/timarney/react-app-rewired/issues/33
+          ...cssRules.loader,
+          { loader: require.resolve('sass-loader'), options: sassLoaderOptions }
+        ]
+      };
+    }
+    else {
+      sassRules = {
+        test: sassExtension,
+        use: [
+          ...cssRules.use,
+          { loader: require.resolve('sass-loader'), options: sassLoaderOptions }
+        ]
+      };
+    }
+
+    const oneOfRule = config.module.rules.find((rule) => rule.oneOf !== undefined);
+    if (oneOfRule) {
+      oneOfRule.oneOf.unshift(sassRules);
+    }
+    else {
+      // Fallback to previous behavior of adding to the end of the rules list.
+      config.module.rules.push(sassRules);
+    }
+
+    return config;
+  }
 }
+
+const rewireSass = createRewireSass();
+
+rewireSass.withLoaderOptions = createRewireSass;
 
 module.exports = rewireSass;
